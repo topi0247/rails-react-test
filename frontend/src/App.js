955fc5369fd1a9ce9259d9a54476447cff9feaab
Kanae-Nishina <karubona.ra247@gmail.com>
2024-03-03 12:14:22 +0900

    set up

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center m-auto p-10">
+      <h1 className='text-2xl'>Hello World!!!!</h1>
+      <section>
+        <p>this is test</p>
+        <button
+          class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+          Button
+        </button>
+      </section>
+    </div>
+  );
+}
+
+export default App;
